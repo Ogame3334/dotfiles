4736af222e4b7c1093430eee7997a78a125fb92d
Ogame3334 <kazuhiroisobe0522@gmail.com>
2026-08-07 00:55:20 +0900

    add: chrome-desktop alias

--- dot_config/bash/bash_alias.bash
+++ dot_config/bash/bash_alias.bash
@@ -5,3 +5,4 @@ alias end-hyprland="command -v hyprshutdown >/dev/null 2>&1 && hyprshutdown || h
 alias nv='nvim'
 alias tty-clock='tty-clock --config ~/.config/tty-clock/config.json'
 alias misskey='/opt/google/chrome/google-chrome --profile-directory=Default --app-id=dakikcbkfolajgahklpjkjlbfhnnfppb "--app-launch-url-for-shortcuts-menu-item=https://misskey.ogamen.cc"'
+alias chrome-desktop='/opt/google/chrome/google-chrome --profile-directory=Default --app-id=cmkncekebbebpfilplodngbpllndjkfo'
